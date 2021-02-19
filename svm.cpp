3d2379ec77aecda3da5cb663decc4dc6e818fb18
Janluke0 <gianlucacaronte@gmail.com>
2021-02-19 14:08:24 +0100

    Improve/get q (#6)
    GET_Q
    
    * .

--- svm.cpp
+++ svm.cpp
@@ -1404,6 +1404,7 @@ public:
 		if ((start = cache->get_data(i, &data, len)) < len)
 		{
 			BEGIN_HOOK(GET_Q);
+#pragma omp parallel for private(j) schedule(static)
 			for (j = start; j < len; j++)
 				data[j] = (Qfloat)(y[i] * y[j] * (this->*kernel_function)(i, j));
 			END_HOOK(GET_Q);
